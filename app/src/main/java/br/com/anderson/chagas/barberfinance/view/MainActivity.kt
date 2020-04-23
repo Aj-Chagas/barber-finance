1e57a24b5fc03f68f02365578bcfc3b2ea51c987
andersonchagas <anderson.1997reis@gmail.com>
2020-04-23 17:14:35 -0300

    splash screen

--- app/src/main/java/br/com/anderson/chagas/barberfinance/view/MainActivity.kt
+++ app/src/main/java/br/com/anderson/chagas/barberfinance/view/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.CustomActivityTheme)
         setContentView(R.layout.activity_main)
         statusBarWhiteWithBackIcons()
         setupNavigation()
